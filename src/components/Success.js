e9f66b325a4e0e3c3d869c4b48c7d36f1dab7fe6
Zachary Soon <zwsoon@gmail.com>
2020-01-11 16:17:16 -0500

    adding firstname, lastname, email to success page

--- src/components/Success.js
+++ src/components/Success.js
@@ -4,30 +4,31 @@ import AppBar from 'material-ui/AppBar';
 
 export class Success extends Component {
     //lets the user go to the next page
-    continue = e =>{
+    continue = e => {
         e.preventDefault();
         // Process Form //
         this.props.nextStep();
     }
     // lets the user go the previous page
-    back = e =>{
+    back = e => {
         e.preventDefault();
         this.props.prevStep();
     }
 
     render() {
         // use values as a variable
+        const { values: { firstName, lastName, email } } = this.props;
         return (
             <MuiThemeProvider>
                 {/*React.Fragment acts like a fake DOM*/}
-               <React.Fragment> 
-                    {/*Create text of success at the top*/} 
-                   <AppBar title="Success!" />
+                <React.Fragment>
+                    {/*Create text of success at the top*/}
+                    <AppBar title="Success!" />
                     {/*Bold text*/}
-                   <h1>Thank You For Your Submission</h1> 
-                   {/*Message of the page*/}
-                   <p>You will get an email with further instructions</p>
-               </React.Fragment>
+                    <h1>Thank you for your submission, {firstName} {lastName}!</h1>
+                    {/*Message of the page*/}
+                    <p>An email will be sent to {email}</p>
+                </React.Fragment>
             </MuiThemeProvider>
         );
     }
@@ -35,7 +36,7 @@ export class Success extends Component {
 
 const styles = {
     button: {
-        margin:15
+        margin: 15
     }
 }
 
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -5,81 +5,87 @@ import Confirm from './Confirm';
 import Success from './Success';
 export class UserForm extends Component {
 
-//When the user types anything in on the UI
-//that string of info will be added to the following state below:
-//check the dev tool under 'Components' in Chrome
-state = {
-    step : 1,
-    firstName: '',
-    lastName: '',
-    email: '',
-    occupation: '',
-    city: '',
-    bio: ''
-}
-
-//Proceed to next step
-nextStep = () =>{
-    //destructuring
-    //grabs step from the state component
-    const{step} = this.state;
-    this.setState({
-        step: step + 1
-    });
-};
-
-// Go back to previous step
-prevStep = () =>{
-    //destructuring
-    const{step} = this.state;
-    this.setState({
-        step: step - 1
-    });
-};
-
-// Handle fields change
-handleChange = input => e => {
-    this.setState({[input]: e.target.value});
-}
-
-// renders the information to the UI
-render() {
-    const {step} = this.state;
-    const {firstName, lastName, email, occupation, city, bio} = this.state;
-    const values = {firstName, lastName, email, occupation, city, bio};
-    switch(step){
-        case 1: 
-            return(
-            <FormUserDetails //calls the FormUserDetailsClass
-                //calls nextstep and increases the step by 1
-                nextStep = {this.nextStep}  //allows the user to go forward        
-                handleChange = {this.handleChange}
-                values = {values}//values in render()
-            />
-            )
-        case 2:
-            return(
-                <FormPersonalDetails //calls the FormPersonalDetailsClass
-                    //calls nextstep and increases the step by 1
-                    nextStep = {this.nextStep} //allows the user to go forward 
-                    prevStep = {this.prevStep} //allows the user to go backwards
-                    handleChange = {this.handleChange}
-                    values = {values}
-                />
-                )
-        case 3:
-            return(
-                <Confirm //calls the FormPersonalDetailsClass
-                    //calls nextstep and increases the step by 1
-                    nextStep = {this.nextStep} //allows the user to go forward 
-                    prevStep = {this.prevStep} //allows the user to go backwards
-                    values = {values}
-                />
-                )
-        case 4:
-            return <Success/>;
+    //When the user types anything in on the UI
+    //that string of info will be added to the following state below:
+    //check the dev tool under 'Components' in Chrome
+    state = {
+        step : 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        occupation: '',
+        city: '',
+        bio: ''
     }
-}
+
+    //Proceed to next step
+    nextStep = () =>{
+        //destructuring
+        //grabs step from the state component
+        const{step} = this.state;
+        this.setState({
+            step: step + 1
+        });
+    };
+
+    // Go back to previous step
+    prevStep = () =>{
+        //destructuring
+        const{step} = this.state;
+        this.setState({
+            step: step - 1
+        });
+    };
+
+    // Handle fields change
+    handleChange = input => e => {
+        this.setState({[input]: e.target.value});
+    }
+
+    // renders the information to the UI
+    render() {
+        const {step} = this.state;
+        const {firstName, lastName, email, occupation, city, bio} = this.state;
+        const values = {firstName, lastName, email, occupation, city, bio};
+
+        //allows the pages
+        switch(step){
+            case 1: 
+                return(
+                <FormUserDetails //calls the FormUserDetailsClass
+                    //calls nextstep and increases the step by 1
+                    nextStep = {this.nextStep}  //allows the user to go forward        
+                    handleChange = {this.handleChange}
+                    values = {values}//values in render()
+                />
+                )
+            case 2:
+                return(
+                    <FormPersonalDetails //calls the FormPersonalDetailsClass
+                        //calls nextstep and increases the step by 1
+                        nextStep = {this.nextStep} //allows the user to go forward 
+                        prevStep = {this.prevStep} //allows the user to go backwards
+                        handleChange = {this.handleChange}
+                        values = {values}
+                    />
+                    )
+            case 3:
+                return(
+                    <Confirm //calls the FormPersonalDetailsClass
+                        //calls nextstep and increases the step by 1
+                        nextStep = {this.nextStep} //allows the user to go forward 
+                        prevStep = {this.prevStep} //allows the user to go backwards
+                        values = {values}
+                    />
+                    )
+            case 4:
+                return(
+                    <Success
+                        values = {values}
+                    />
+                )
+        };
+    }
 }
 
 export default UserForm
